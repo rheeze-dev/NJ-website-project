1f430ced22658e2de6dcbff65789cba67ae87e39
rheeze-dev <ggybzz2297@gmail.com>
2024-09-20 19:54:02 -0400

    implement capital page

--- capital.js
+++ capital.js
@@ -1,6 +1,5 @@
 let slideIndex = 0;
 showSlides();
-getNewsApi();
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -18,50 +17,6 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
-
-let i = 0; // Declare i outside the function to keep its value across function calls
-
-async function getNewsApi() {
-  const response = await fetch(
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=212ca14d0aad4987b0759b2f4177c1e3",
-    {
-      method: "GET",
-      headers: {
-        "X-Api-Key": "212ca14d0aad4987b0759b2f4177c1e3",
-      },
-    }
-  );
-
-  if (!response.ok) {
-    document.querySelector(
-      "#news-api"
-    ).innerHTML = `<div class="error-message">Please check your internet connection!</div>`;
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  const data = await response.json();
-
-  // Display the news article at index i
-  document.querySelector(
-    "#content-api"
-  ).innerHTML = `<h4>${data.articles[i].description}</h4>`;
-
-  document.querySelector("#info-api").innerHTML = `<h4>Published by ${
-    data.articles[i].source.name
-  } on ${new Date(data.articles[i].publishedAt).toLocaleDateString()}.`;
-  document.querySelector(
-    "#author-api"
-  ).innerHTML = `<h4>Authors: ${data.articles[i].author}.
-  <a href="${data.articles[i].url}" target="_blank"> Click here to check news.</a></h4></h4>`;
-  // Increase i after 10 seconds, and loop back if i exceeds the article count
-  setTimeout(() => {
-    i++;
-    if (i >= data.articles.length) {
-      i = 0; // Reset i to 0 when it reaches the end of the list
-    }
-    getNewsApi(); // Recursive call to keep updating the news
-  }, 1000000); // 10 seconds interval
 }
 
 document.querySelector(
@@ -121,15 +76,3 @@ Object.values(navLinks).forEach((link) => {
 
 // Event listener for URL changes (for back/forward navigation)
 window.addEventListener("popstate", updateActiveClass);
-
-// document.getElementById("menu-toggle").addEventListener("click", function () {
-//   const navbar = document.getElementById("navbar");
-//   // Toggle the display property between 'none' and 'block'
-//   if (navbar.style.display === "none" || navbar.style.display === "") {
-//     navbar.style.display = "block"; // Show the navbar
-//     this.textContent = "Hide Navbars"; // Change button text
-//   } else {
-//     navbar.style.display = "none"; // Hide the navbar
-//     this.textContent = "Show Navbars"; // Change button text
-//   }
-// });
